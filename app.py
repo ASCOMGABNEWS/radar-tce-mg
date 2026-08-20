0e30defd76c2376657e9ca0003fdf54f4a45f174
ASCOMGABNEWS <ascom@agostinhopatrus.com.br>
2026-08-20 16:21:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -32,10 +32,8 @@ from reportlab.lib.units import cm
 
 if "OPENAI_API_KEY" in st.secrets:
     client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-    st.success("🤖 IA conectada com sucesso!")
 else:
     client = None
-    st.warning("🤖 Chave da IA não encontrada.")
 
 
 def esc_html(value):
